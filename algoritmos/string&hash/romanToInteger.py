cf5cd9e0a5dc84ee9454a5940d336d2db5b7333f
Francapaa <franciscocaparruva@gmail.com>
2026-05-17 22:55:15 -0300

    added romanToInteger and searchInsertPosition

---
+++ algoritmos/string&hash/romanToInteger.py
@@ -0,0 +1,18 @@
+def romanToInt(self, s: str) -> int:
+        values= {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        total = 0
+        for i in range (len(s)):
+            currentValue = values[s[i]]
+            if i + 1 < len(s) and currentValue < values[s[i+1]]:
+                total -= currentValue
+            else:
+                total+= currentValue
+        return total
